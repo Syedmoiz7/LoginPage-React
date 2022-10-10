39eead1d088cddeb97b9c057dd28545d5fb6c62a
jatindarkhatry <khatrij784@gmail.com>
2022-10-10 23:12:03 +0500

    first commit

---
+++ src/components/login/index.jsx
@@ -0,0 +1,25 @@
+import './index.css';
+// import { useState } from 'react';
+// import axios from "axios"
+
+function Login() {
+
+    return (
+        <div className='mainCont'>
+            <div className='image'>
+                <img src="https://colorlib.com/etc/lf/Login_v18/images/bg-01.jpg" alt="Side background" className='img' />
+            </div>
+            <div className="loginForm">
+                <h1>Login to continue</h1>
+            </div>
+
+
+        </div>
+    )
+}
+
+
+
+export default Login;
+
+
